9b3323b80e38363cc859fd77ba5ffa436f1c129d
Mahimai Raja J <mahimairaja3@gmail.com>
2023-07-26 22:16:53 +0530

    front-end ui

--- utils/user_interface.py
+++ utils/user_interface.py
@@ -1,9 +1,6 @@
 import streamlit as st
 
 
-
 def load_css_file(css_file_path):
-    with open(css_file_path, encoding='UTF-8') as f:
+    with open(css_file_path, encoding="UTF-8") as f:
         return st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-
-
